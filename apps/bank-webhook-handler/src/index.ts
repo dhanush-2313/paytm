853ab011ec123a44ff5132aaf95d33a9b3edfea4
dhanush-2313 <dhanusu006@gmail.com>
2024-09-19 21:54:48 +0530

    components dashboard

--- apps/bank-webhook-handler/src/index.ts
+++ apps/bank-webhook-handler/src/index.ts
@@ -11,8 +11,6 @@ interface paymentInformationType {
 }
 
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
-    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
     const paymentInformation = <paymentInformationType>{
         token: req.body.token,
         userId: req.body.user_identifier,
